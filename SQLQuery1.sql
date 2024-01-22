299ee814ea3351a1825f72594c07e97237a62deb
Burhanaddinzm <burhanaddinzm@code.edu.az>
2024-01-22 22:43:45 +0400

    SQL Intro

---
+++ SQLQuery1.sql
@@ -0,0 +1,39 @@
+--CREATE DATABASE Academy
+--DROP DATABASE Academy
+
+--CREATE DATABASE Academy
+--USE Academy
+
+--Create TABLE Students
+--(
+--[Name] VARCHAR(30),
+--Surname VARCHAR(30),
+--Age INT,
+--[Group] VARCHAR(30)
+--)
+
+--Create TABLE Teachers
+--(
+--[Name] VARCHAR(30),
+--Surname VARCHAR(30),
+--[Group] VARCHAR(30)
+--)
+
+--INSERT INTO Students
+--VALUES
+--('Burhanaddin','Mirsadizada',21,'P237'),('Ramazan','Shirinov',15,'P237')
+
+--INSERT INTO Teachers
+--VALUES
+--('Huseyn','Mirzayev','P237'),('Ahad','Tagiyeh','P237')
+
+--UPDATE Students
+--SET Age=18
+--WHERE [Name]='Ramazan'
+
+--DELETE Teachers
+--WHERE [Name]='Ahad'
+
+SELECT * FROM Teachers
+SELECT * FROM Students
+WHERE [Name]='Ramazan'
